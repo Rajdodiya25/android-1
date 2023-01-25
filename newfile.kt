89ac150465eb28490f9b0f7658f5f5c5fb059042
Rajdodiya25 <123078330+Rajdodiya25@users.noreply.github.com>
2023-01-25 17:35:18 +0530

    Add files via upload

---
+++ newfile.kt
@@ -0,0 +1,11 @@
+fun main()
+
+
+{
+    println("enter your number")
+    var a= readLine() !!.toFloat()
+    var b=a*a*a
+    var c=a*a
+    println("your number square : $c")
+    println("your number cube : $b")
+}
