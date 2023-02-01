9895b825563c7f4760ad6de2f57ccc364eaf4dd5
Rajdodiya25 <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-01 23:35:09 +0530

    Delete kotlin directory

--- kotlin/square & cube.kt
+++
@@ -1,9 +0,0 @@
-fun main()
-{
-    println("enter your number")
-    var l= readLine()!!.toFloat()
-    var m=l*l*l
-    var n=l*l
-    println("your number square : $m")
-    println("your number cube  : $n")
-}
