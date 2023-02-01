025dc93b0c8797cb35ce1830874f2c35a9547355
Rajdodiya25 <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-01 23:30:34 +0530

    Add files via upload

---
+++ interest.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    print("enter your N  : ")
+    var N= readLine()!!.toFloat()
+    print("enter your M  : ")
+    var M= readLine()!!.toFloat()
+    print("enter your E  : ")
+    var E= readLine()!!.toFloat()
+    var I=N * M * E/100
+    println("simple interest is : $I ")
+}
