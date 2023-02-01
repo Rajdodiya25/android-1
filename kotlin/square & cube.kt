eb580ff1b0f9e05de27002383e73870bebd2dd4d
Rajdodiya25 <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-01 23:34:34 +0530

    Add files via upload

---
+++ kotlin/square & cube.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    println("enter your number")
+    var l= readLine()!!.toFloat()
+    var m=l*l*l
+    var n=l*l
+    println("your number square : $m")
+    println("your number cube  : $n")
+}
