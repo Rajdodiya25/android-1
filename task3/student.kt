946be0c7e734ea10a9d2235ef7e246c9c0cbb66f
Raj Dodiya <123078330+Rajdodiya25@users.noreply.github.com>
2023-02-06 23:03:52 +0530

    Add files via upload

---
+++ task3/student.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var name="raj dodiya"
+    var dbirth=" 01-jan 2004"
+    var age =19
+    var address="junaghdh,mangrol shepa "
+
+
+    println("your name is $name and your birth of date $dbirth ")
+    println(" your age is $age and your courent address is $address")
+}
